b149674a066e060ee4a0d6d46701934c405ad208
ahengine <ahengine@live.com>
2023-11-19 21:53:02 +0330

    remove XAPIGenerator and update Connection Checker

--- Runtime/Utilities/ConnectionChecker.cs
+++ Runtime/Utilities/ConnectionChecker.cs
@@ -1,36 +1,35 @@
 using UnityEngine;
 using IEnumerator = System.Collections.IEnumerator;
 
-namespace Networking.WebRequestHandler
+namespace HTTPRequestService.Helpers
 {
     public class ConnectionChecker : MonoBehaviour
     {
-        public static int CheckingTime = 5;
+        private static ConnectionChecker instance;
 
-        public static void SetCheckingTime(int checkingTime) =>
-            CheckingTime = checkingTime;
+        private void OnDestroy() {
+            instance = null;
+        }
+
+        public static int CheckingTime { private set; get; } = 5;
+        public static void SetCheckingTime(int value) =>
+            CheckingTime = value;
 
         public static bool AllowCheckingConnection { private set; get; } = true;
-
         public static void SetAllowCheckingConnection(bool value) =>
             AllowCheckingConnection = value;
 
+
+        public static void StartChecking()
+        {
+            if (!instance)
+                instance = new GameObject(typeof(ConnectionChecker).Name).AddComponent<ConnectionChecker>();
+
+            instance.StopAllCoroutines();
+            instance.StartCoroutine(Checking());
+        }
+
         public static bool IsConnected { private set; get; }
-
-        [RuntimeInitializeOnLoadMethod]
-        private static void StartChecking()
-        {
-            if (GameObject.FindObjectOfType<ConnectionChecker>())
-                return;
-
-            new GameObject(typeof(ConnectionChecker).Name).AddComponent<ConnectionChecker>();
-        }
-
-        private void Start()
-        {
-            StartCoroutine(Checking());
-        }
-
         private static IEnumerator Checking()
         {
             while (true)
@@ -42,7 +41,7 @@ namespace Networking.WebRequestHandler
                     while (!request.isDone)
                         yield return new WaitForEndOfFrame();
 
-                    // IsConnected = request. == null;
+                    //IsConnected = request.time
                 }
             }
         }
